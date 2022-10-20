7f467ab6d588dbaf16d4538a37e2fe1fde0999e6
abbjetmus <jeton.mustini@abbindustrigymnasium.se>
2022-10-20 12:03:20 +0200

    latest

--- Projekt-Temperatur-Sensor/Exempel/index.js
+++ Projekt-Temperatur-Sensor/Exempel/index.js
@@ -1,17 +1,47 @@
-let database = firebase.database()
+// Your web app's Firebase configuration
 
-let person = database.ref('/person').once('value').then(function(pelle)  {
-  console.log(pelle.val().firstname)
-})
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  databaseURL: "",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+var ledRef = db.ref("led");
+
+ledRef.once("value").then((snapshot) => {
+  if (snapshot.val()) {
+    let myCheckBox = document.getElementById("checkbox");
+    myCheckBox.checked = snapshot.val().led;
+    if (myCheckBox.checked) {
+      document.getElementById("myImage").src = "light-bulb-on.png";
+    } else {
+      document.getElementById("myImage").src = "light-bulb-off.png";
+    }
+  }
+});
+
+ledRef.on("value", (snapshot) => {
+  const data = snapshot.val();
+});
 
 function turnOnOffLight() {
-  let myCheckBox = document.getElementById('checkbox')
+  let myCheckBox = document.getElementById("checkbox");
 
   if (myCheckBox.checked) {
-    document.getElementById('myImage').src='light-bulb-on.png'
-    database.ref('/led').update({led:false})
+    document.getElementById("myImage").src = "light-bulb-on.png";
+    ledRef.update({ led: true });
   } else {
-    document.getElementById('myImage').src='light-bulb-off.png'
-    database.ref('/led').update({led:true})
+    document.getElementById("myImage").src = "light-bulb-off.png";
+    // database.ref("/led").update({ led: true });
+    ledRef.update({ led: false });
   }
 }
