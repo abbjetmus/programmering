0435fdbf9173bbe6cdff865db8efceaccea7eb6c
abbjetmus <jeton.mustini@abbindustrigymnasium.se>
2022-10-25 09:20:18 +0200

    lastest

--- Projekt-Temperatur-Sensor/Exempel/index.js
+++ Projekt-Temperatur-Sensor/Exempel/index.js
@@ -2,14 +2,13 @@
 // https://www.survivingwithandroid.com/esp8266-firebase-realtime-database-iot-controlled-rgb-leds/
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAK8AJmJPmUrHjPvKl486i99SE_Wfk0R_4",
-  authDomain: "temp-projekt-220s.firebaseapp.com",
-  projectId: "temp-projekt-220s",
-  storageBucket: "temp-projekt-220s.appspot.com",
-  messagingSenderId: "401787728103",
-  appId: "1:401787728103:web:3f9f7614f1dae5d1c13242",
-  databaseURL:
-    "https://temp-projekt-220s-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  databaseURL: "",
 };
 
 // Initialize Firebase
