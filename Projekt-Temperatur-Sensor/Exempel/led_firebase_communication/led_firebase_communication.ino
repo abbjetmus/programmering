793ff0f0443827ea3a19382086433b163c434ac5
hgjetmus <jeton.mustini@hitachigymnasiet.se>
2024-11-07 09:25:07 +0100

    Update temp instructions

--- Projekt-Temperatur-Sensor/Exempel/led_firebase_communication/led_firebase_communication.ino
+++ Projekt-Temperatur-Sensor/Exempel/led_firebase_communication/led_firebase_communication.ino
@@ -1,55 +1,62 @@
-#include "FirebaseESP8266.h"
-#include <ESP8266WiFi.h>
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
+#include "FirebaseESP8266.h"  // Inkluderar biblioteket för Firebase
+#include <ESP8266WiFi.h>      // Inkluderar biblioteket för WiFi-funktioner
 
-#define WIFI_SSID "ABB_Gym_IOT"
-#define WIFI_PASSWORD "Welcome2abb"
-//Define Firebase Data objects
-FirebaseData firebaseData1;
-int counter = 0;
+// Definiera Firebase-host och autentisering
+#define FIREBASE_HOST ""      // Här ska du ange din Firebase-host dvs url till databasen
+#define FIREBASE_AUTH ""      // Här ska du ange din Firebase-autentisering dvs web api nyckeln
 
+// Definiera WiFi-uppgifter
+#define WIFI_SSID "ABB_Gym_IOT"  // WiFi-namn
+#define WIFI_PASSWORD "Welcome2abb" // WiFi-lösenord
+
+// Skapa Firebase Data-objekt
+FirebaseData firebaseData;
+int counter = 0; // En räknare för att hålla koll på loopens iterationer
 
 void setup() {
-  Serial.begin(115200);
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.begin(115200); // Startar seriell kommunikation med en baudrate på 115200
+  pinMode(LED_BUILTIN, OUTPUT); // Sätter den inbyggda LED:n som utgång
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // Ansluter till WiFi-nätverket
   Serial.print("Connecting to Wi-fi");
+  
+  // Väntar på att WiFi ska ansluta
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(300);
+    Serial.print("."); // Skriver ut punkter medan den väntar
+    delay(300); // Väntar 300 ms
   }
+  
   Serial.println();
-  Serial.print("Connected with IP: ");
+  Serial.print("Connected with IP: "); // Skriver ut den lokala IP-adressen
   Serial.println(WiFi.localIP());
   Serial.println();
   
+  // Initierar Firebase med host och autentisering
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.reconnectWiFi(true);
+  Firebase.reconnectWiFi(true); // Återansluter till WiFi automatiskt vid förlust
 }
 
 void loop() {
-  delay(5000);
-  counter = counter + 1;
-  // put your main code here, to run repeatedly:
+  delay(5000); // Väntar 5 sekunder mellan varje iteration
+  counter = counter + 1; // Ökar räknaren med 1
 
-  if (Firebase.getBool(firebaseData1, "/led/led")) {
-      if (firebaseData1.dataType() == "boolean")
-      {         
-          Serial.println(firebaseData1.boolData());
-          digitalWrite(LED_BUILTIN, firebaseData1.boolData());
+  // Hämtar en boolean från Firebase för att styra LED:n
+  if (Firebase.getBool(firebaseData, "/led/led")) {
+      if (firebaseData.dataType() == "boolean") {         
+          Serial.println(firebaseData.boolData()); // Skriver ut värdet från Firebase
+          digitalWrite(LED_BUILTIN, firebaseData.boolData()); // Sätter LED:n baserat på värdet
       }
   }
 
-  if (Firebase.getInt(firebaseData1, "/counter")) {
-   if  (firebaseData1.dataType() == "int") {
-      int val = firebaseData1.intData();
-      Serial.println(val);
-      delay(3000);
-      Firebase.setInt(firebaseData1, "/counter", 60);
+  // Hämtar ett heltal från Firebase och skriver ut det
+  if (Firebase.getInt(firebaseData, "/counter")) {
+   if (firebaseData.dataType() == "int") {
+      int val = firebaseData.intData(); // Hämtar heltalsvärdet
+      Serial.println(val); // Skriver ut värdet
+      delay(3000); // Väntar 3 sekunder
+      Firebase.setInt(firebaseData, "/counter", 60); // Sätter värdet till 60 i Firebase
     }
   }
 
-  Firebase.setInt(firebaseData1, "/Room/2022-10-25/Temp/1210",5);
+  // Sätter ett heltal i Firebase under en specifik väg
+  Firebase.setInt(firebaseData, "/Room/2022-10-25/Temp/1210", 5);
 }
--- Projekt-Temperatur-Sensor/README.md
+++ Projekt-Temperatur-Sensor/README.md
@@ -13,6 +13,32 @@ Där finns beskrivning på vilken funktionalitet som det stöder och hur man anv
 <br>
 <br>
 <br>
+För att kommunicera med Firebase måste man ha ett bibliotek för det både i Arduino koden och på hemsidan. För Arduino koden är det viktigt att fäljande version används som på bilden.
+
+Det som är nytt är att man måste använda firebase biblioteket på Arduino som heter så här
+
+![Image](./Exempel/esp8266firebaseclient.png)
+
+För webbsidan kan ni bara bygga vidare på mitt exempel som har biblioteket inkluderat som script-taggar.
+
+
+I mappen Exempel finns exempel kod för både webb delen och arduino bitarna som man an använda.
+
+Det är mycket kommentarer i arduino koden, så behöver ni inte skriva och kan radera kommentarerna om ni vill, de är mest för ni ska förstå koden.
+Ni måste använda FIREBASE_HOST och FIREBASE_AUTH från ert firebase projekt enligt bilderna.
+
+```c++
+// Definiera Firebase-host och autentisering
+#define FIREBASE_HOST ""      // Här ska du ange din Firebase-host dvs url till databasen
+#define FIREBASE_AUTH ""      // Här ska du ange din Firebase-autentisering dvs web api nyckeln
+```
+
+![Image](./Exempel/web-api-key.png)
+
+![Image](./Exempel/database-url.png)
+
+Testa gärna hela flödet med exemplet att ni kan slå av och på lampan via hemsidan.
+
 Länken nedan visar hur man sätter upp ett Firebase projekt.
 <br><br>
 <https://medium.com/@vibrologic/serverless-iots-with-firebase-realtime-database-and-esp8266-9937d98d1ae0>
@@ -35,19 +61,6 @@ Det här är en introduktionshandledning som behandlar grunderna för Firebase-p
 Här nedan är dokumentation från Firebase egna hemsida om hur man använder produkten.<br><br>
 <https://firebase.google.com/docs/database>
 
-Det som är nytt är att man måste använda firebase biblioteket på Arduino som heter så här
-
-![Image](./Exempel/esp8266firebaseclient.png)
-
-Därefter
-
-```c++
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
-
-#define WIFI_SSID "ABB_Gym_IOT"
-#define WIFI_PASSWORD "Welcome2abb"
-```
 
 ## Hemsidan
 Ni kan välja att bygga er hemsida med endast HTML, CSS och JavaScript. Men det kan vara lite svårt att designa en hemsida och därför brukar man använda någon form av CSS-bibliotek. Ett sådant bibliotek är tex Bootstrap som jag kan rekommendera att ni använder i era projekt.
